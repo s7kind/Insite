695a12532e0a953419eab47d9841d3793184863d
s7kind <s7kind@gmail.com>
2019-03-06 20:54:20 +0200

    add dark
    add dark

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,22 @@
 'use strict';
+
+
+const svgLight = document.querySelector('.svg_bg');
+const svgDark = document.querySelector('.svg_bg_dark');
+
+svgLight.addEventListener('click', function (event) {
+    this.classList.add('no_active');
+    svgDark.classList.add('active');
+    setTimeout(()=> {
+        document.body.classList.add('dark');
+    }, 1000);
+});
+
+svgDark.addEventListener('click', function (event) {
+    svgLight.classList.remove('no_active');
+    svgDark.classList.remove('active');
+    setTimeout(()=> {
+        document.body.classList.remove('dark');
+    }, 1000);
+});
+
